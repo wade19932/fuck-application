be21040d96919e3038deef197a722c0e6f74c669
wade <wade@wadedeMacBook-Air.local>
2020-05-23 11:19:21 +0800

    init repo

---
+++ src/main/java/motherfuck/FuckApplication.java
@@ -0,0 +1,14 @@
+package motherfuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/motherfuck/FuckApplicationTests.java
@@ -0,0 +1,13 @@
+package motherfuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
