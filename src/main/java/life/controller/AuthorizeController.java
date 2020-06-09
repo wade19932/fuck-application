190c87cf24b7b7cd45a5005d418755205251d09b
Wade <453859153@qq.com>
2020-06-09 21:49:23 +0800

    chaged

--- src/main/java/life/controller/AuthorizeController.java
+++ src/main/java/life/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import life.provider.GithubProvider;
+import sun.net.httpserver.HttpServerImpl;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -25,23 +28,31 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state
-    ){
-        AccesstTkenDTO accesstTkenDTO = new AccesstTkenDTO();
-        accesstTkenDTO.setClient_id(clientId);
-        accesstTkenDTO.setClient_secret(clientSecret);
-        accesstTkenDTO.setCode(code);
-        accesstTkenDTO.setRedirect_uri(clientUri);
-        accesstTkenDTO.setState(state);
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
-        String accessToken = githubProvider.getAccessToken(accesstTkenDTO);
-        System.out.println(accessToken);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
 
-        return "index";
+
+            AccesstTkenDTO accesstTkenDTO = new AccesstTkenDTO();
+            accesstTkenDTO.setClient_id(clientId);
+            accesstTkenDTO.setClient_secret(clientSecret);
+            accesstTkenDTO.setCode(code);
+            accesstTkenDTO.setRedirect_uri(clientUri);
+            accesstTkenDTO.setState(state);
+
+            String accessToken = githubProvider.getAccessToken(accesstTkenDTO);
+            System.out.println(accessToken);
+            GithubUser user = githubProvider.getUser(accessToken);
+            if (user != null) {
+                request.getSession().setAttribute("user",user);
+                return "redirect:/";
+                //登陆成功
+            } else {
+                return "redirect:/";
+                //登陆失败，重新登陆
+
+            }
+        }
+
+
     }
-
-
-
-}
